9b83c5423627d33c68e792597154b8512d498d27
TeeCee-I <teeceeiheukwumere@gmail.com>
2022-09-09 02:25:40 -0700

    C program that prints the text

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,8 @@
+#!/bin/bash
+/* A C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line.*/
+#include <stdio.h>
+int main( void )
+	{
+		puts("\"Programming is like building a multilingual puzzle");
+			return (0);
+	}
