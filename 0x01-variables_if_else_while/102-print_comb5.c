1f9c477329c08e90dd442f8166d7e9796c6f8bd7
TeeCee-I <teeceeiheukwumere@gmail.com>
2022-09-09 19:24:50 -0700

    Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -5,39 +5,28 @@
  */
 int main(void)
 {
-	int a, b, c, d;
+	int a, b;
 
-	for (a = 0; a <= 9; a++)
+	for (a = 0; a < 100; a++)
 	{
-		for (b = 0; b <= 9; b++)
+		for (b = 0; b < 100; b++)
 		{
-			for (c = a; c <= 9; c++)
+			if (a < b)
 			{
-				for (d = (d == 10 && b == 0) ? 0 : (b + 1); d <= 9; d++)
-				{
-					putchar((a % 10) + '0');
-					putchar((b % 10) + '0');
-					putchar(' ');
-					putchar((c % 10) + '0');
-					putchar((d % 10) + '0');
+				putchar((a / 10) + 48);
+				putchar((a % 10) + 48);
+				putchar(' ');
+				putchar((b / 10) + 48);
+				putchar((b % 10) + 48);
 
-					if (a == 9 && b == 8 && c == 9 && d = 9)
-						break;
-
+					if (a != 98 || b != 99)
+					{
 					putchar(',');
 					putchar(' ');
-				}
-
-				if (a == 9 && b == 8 && c == 9 && d == 9)
-					break;
+					}
 			}
 
-			if (a == 9 && b == 8 && c == d == 9)
-				break;
 		}
-
-		if (a == 9 && b == 8 && c == 9 && d == 9)
-			break;
 	}
 
 	putchar('\n');
