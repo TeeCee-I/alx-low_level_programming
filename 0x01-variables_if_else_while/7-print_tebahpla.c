17f6680b21377f592e210d238e2cd437d3dfe7dc
TeeCee-I <teeceeiheukwumere@gmail.com>
2022-09-09 17:12:35 -0700

    7. Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Prints the lowercase alphabet in reverse
+ * Return: Returns true always
+ */
+int main(void)
+{
+	char alpha = 122;
+
+	whhile (alpha >= 97)
+	{
+		putchar(alpha--);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
