1a2dba3c0f208d3d9475046ca0b7d08d3eeaf384
TeeCee-I <teeceeiheukwumere@gmail.com>
2022-09-13 09:40:26 -0700

    2. 10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,14 +6,15 @@
  */
 void print_alphabet_x10(void)
 {
-	char c;
 	int n;
 
 	for (n = 1; n <= 10; n++)
 			
 		{
+			char c;
+
 			for (c = 'a'; c <= 'z'; c++)
-			{	
+				{	
 					_putchar(c);
 					_putchar('\n');
 			}
