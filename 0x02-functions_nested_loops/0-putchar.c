19f11245c88677214f3aa6b199d606a3d0840eb7
TeeCee-I <teeceeiheukwumere@gmail.com>
2022-09-13 08:34:25 -0700

    a program that prints _putchar, followed by a new line

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <main.h>
+
+/**
+ * calling declared header
+ * replaced stdio with declared header
+ * a program that prints _putchar, followed by a new line
+ * The program returns zero
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
