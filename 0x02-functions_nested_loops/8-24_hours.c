c73f9c14dbaf32f38e3e6a0d2ec0d8e4173e2168
TeeCee-I <teeceeiheukwumere@gmail.com>
2022-09-14 06:19:58 -0700

    jack_bauer(void)

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59
+ * Return: Returns declared type void always
+ */
+void jack_bauer(void)
+{
+	int a, b, c, d;
+
+	for (a = 48; a <= 50; a++)
+	{
+			for (b = 48; b <= 57; b++)
+			{
+					for (c = 48; c <= 53; c++)
+					{
+						if ((a <= 49 && b <= 57) || (a <= 50 && b <= 51)
+						{
+								
+								for (d = 48; d <= 57; d++)
+								{
+								_putchar(a);
+								_putchar(b);
+								_putchar(':');
+								_putchar(c);
+								_putchar(d);
+								_putchar('\n');
+								}
+						}
+					}
+			}
+	}
+}
+
+								
