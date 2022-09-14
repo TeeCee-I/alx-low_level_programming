bb78509bee04e5ef0a0ccadf938b31bf4801a82a
TeeCee-I <teeceeiheukwumere@gmail.com>
2022-09-14 04:57:27 -0700

    5. Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_sign - a function that prints the sign of a number
+ * @c: initialized value
+ * Return: Returns 1, 0, -1, for +ve, zero, -ve arguments respectively
+ */
+int print_sign(int c)
+{
+	if (c < 0)
+		{
+			_putchar('-');
+				return (-1);
+		}
+	else if (c == 0)
+		{
+			_putchar('0');
+				return (0);
+		}
+	else
+		{
+			_putchar('+');
+				return (1);
+												}
+}
