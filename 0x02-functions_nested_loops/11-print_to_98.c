826f1f9f669d3598a518d52180ebddb072477118
TeeCee-I <teeceeiheukwumere@gmail.com>
2022-09-14 10:01:32 -0700

    11. 98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,12 +2,13 @@
 #include "main.h"                                                                                                                       
                                                                                                                                         
 /**                                                                                                                                     
- *  * print_to_98 - prints all natural numbers from n to 98                                                                                
- *   * @n: Number input                                                                                                                     
- *    * Return: Returns void always                                                                                                          
- *     */                                                                                                                                     
+ * print_to_98 - prints all natural numbers from n to 98                                                                                
+ * @n: Number input                                                                                                                     
+ * Return: Returns void always                                                                                                          
+ */                                                                                                                                     
 void print_to_98(int n)                                                                                                                 
 {
-	for (n = 0; n <= 98; n++)                                                                                                       
-		printf("%d,\n", n);                                                                                                     
-}     
+	for (n = 0; n <= 98; n++){                               
+		printf("%d, ", n);
+	}
+}
