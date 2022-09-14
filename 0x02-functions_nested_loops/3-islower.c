a7cab501b634bb4d2ae3cd6b84bc956c5292a62b
TeeCee-I <teeceeiheukwumere@gmail.com>
2022-09-14 04:20:27 -0700

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,8 +3,7 @@
 /**
  * _islower - a function that checks for lowercase character
  * @c: ASCII of alphabet range
- * Return: Returns 1 if lowercase
- * Return: Returns 0 if not
+ * Return: Returns 1 if lowercase and Returns 0 if not
  */
 int _islower(int c)
 {
