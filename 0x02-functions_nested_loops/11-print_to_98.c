b1a51e9edb46c0f58c74d78b0d440dd2af136ae7
TeeCee-I <teeceeiheukwumere@gmail.com>
2022-09-14 08:39:37 -0700

    11. 98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,6 +1,6 @@
+#include <stdio.h>
 #include "main.h"
 
-void nlt98(int);
 /**
  * print_to_98 - prints all natural numbers from n to 98
  * @n: Number input
@@ -8,37 +8,9 @@ void nlt98(int);
  */
 void print_to_98(int n)
 {
-	if (n == 98)
+	int n;
 	{
-		_putchar('9');
-		_putchar('8');
+		for (n = 0; n <= 98; n++)
+			printf("%d,\n", n);
 	}
-
-	nlt98(n);
-
-	if (n > 98)
-	{
-		for (; n >= 98; n--)
-		{
-			if (n < 100)
-			{
-				_putchar((n / 10) + '0');
-				_putchar((n % 10) + '0');
-			}
-			else if (n >= 100)
-			{
-				_putchar((n / 100) + '0');
-				_putchar(((n / 10) % 10) + '0');
-				_putchar((n % 10) + '0');
-			}
-
-			if (n == 98)
-				break;
-			
-			_putchar(',');
-			_putchar(' ');
-		}
-	}
-
-	_putchar('\n');
 }
