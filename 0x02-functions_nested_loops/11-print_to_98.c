db5d55609e77fb2ce0ec11298c2e75863eeb7e9c
TeeCee-I <teeceeiheukwumere@gmail.com>
2022-09-14 10:15:32 -0700

    11. 98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,7 +8,8 @@
  */                                                                                                                                     
 void print_to_98(int n)                                                                                                                 
 {
-	for (int i = n; i <= 98; i++){                               
+	int i;
+	for (i = n; i <= 98; i++){                               
 		printf("%d, ", i);
 	}
 }
