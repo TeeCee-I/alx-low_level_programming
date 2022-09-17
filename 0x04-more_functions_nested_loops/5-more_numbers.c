af4d4d6eeb8fe8c32a85258b8eaf5e87f91309d0
TeeCee-I <teeceeiheukwumere@gmail.com>
2022-09-16 17:23:31 -0700

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * more_numbers - Prints nums 0 - 14 10 times
+ * Return: 0 Always
+ */
+void more_numbers(void)
+{
+	int x, y;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 0; y <= 14; y++)
+		{
+			if (y > 9)
+				_putchar((y / 10) + '0');
+
+			_putchar((y % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
