a87bcb94d5a20e2d42ad149bfb26db52635817d5
TeeCee-I <teeceeiheukwumere@gmail.com>
2022-09-27 18:10:05 -0700

    100-set_string.c

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * set_string - set a string to different string
+ * @s: the string
+ * @to: the final string
+ * Return: Void
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
