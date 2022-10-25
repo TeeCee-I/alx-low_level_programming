baca511c02e5085565ba298f2dd284d9034bfd73
TeeCee-I <teeceeiheukwumere@gmail.com>
2022-10-25 07:44:53 -0700

    removed

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,14 +1,1 @@
-#include <stdio.h>
-
-/**
- * main - Identifier to Print its name
- * @argc: Arg count
- * @argv: Args
- * Return: 0
- */
-int main(__attribute__((unused)) int argc, char *argv[])
-{
-	printf("%s\n", argv[0]);
-
-	return (0);
-}
+exit
